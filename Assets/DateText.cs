cfe6290e39c0d9441146bacec666a9c51617e3de
Seyda Nur DEMIR <dmrsydnr@gmail.com>
2020-08-15 00:35:34 +0300

    Date Time Text Added

---
+++ Assets/DateText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DateText : MonoBehaviour
+{
+    public GameObject DateTextObject;
+
+    public int day;
+    public int month;
+    public int year;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        day = System.DateTime.Now.Day;
+        month = System.DateTime.Now.Month;
+        year = System.DateTime.Now.Year;
+
+        DateTextObject.GetComponent<Text>().text = "DATE : " + day + "/" + month + "/" + year;
+    }
+}
---
+++ Assets/TimeText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeText : MonoBehaviour
+{
+    public GameObject TimeTextObject;
+
+    public int hour;
+    public int minute;
+    public int second;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        minute = System.DateTime.Now.Minute;
+        second = System.DateTime.Now.Second;
+        hour = System.DateTime.Now.Hour;
+
+        TimeTextObject.GetComponent<Text>().text = "TIME : " + hour + ":" + minute + ":" + second;
+    }
+}
