91c450d9d661680d6ce1a4cd093de7c931ad07d9
Muhammetalp <mamikalp61@gmail.com>
2020-08-22 16:37:40 +0300

    Simulation Mode - Hardware Mode Seperation.

---
+++ Assets/Scripts/Simulation Mode/Sim_PlexiMovement.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sim_PlexiMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject BallHeightObject;
+    [SerializeField] private GameObject BallLocationHandlerObject;
+    [SerializeField] private GameObject UIObject;
+    [SerializeField] private GameObject GraphObject;
+    [SerializeField] private GameObject ballObject;
+    private Transform tForm;
+    private Rigidbody rBody;
+    private bool moveUp;
+    private float vibrationTimer;
+    private float rotateTimer;
+
+    private Sim_BallHeightHandler bhHandler;
+    private Sim_BallLocationHandler blHandler;
+    private Sim_UIHandler uiHandler;
+    private Sim_DrawGraph drawGraph;
+    private Transform ballTransform;
+    private Rigidbody ballRigid;
+
+    public float xMaxValue;
+    public float xMinValue;
+
+    public float yMaxValue;
+    public float yMinValue;
+
+    public float distanceMaxValue;
+    public float distanceMinValue;
+
+    public float originXValue;
+    public float originYValue;
+
+    public float rotatePositionValue = 0.003f;
+    public float rotateVelocityValue = 0.01f;
+
+    private float reversedistanceFactor = 100f / 5f;
+    private float reversexFactor = 500f / 3f;
+    private float reverseyFactor = 500f / 3f;
+
+    private float currentXConverted;
+    private float currentYConverted;
+    private float currentZConverted;
+
+    private void Awake()
+    {
+        xMaxValue = 500f;
+        xMinValue = 0f;
+
+        yMaxValue = 500f;
+        yMinValue = 0f;
+
+        distanceMaxValue = 100f;
+        distanceMinValue = 0f;
+
+        originXValue  = (xMaxValue - xMinValue ) / 2f;
+        originYValue = (yMaxValue - yMinValue) / 2f;
+
+        reversedistanceFactor = distanceMaxValue / 5f;
+        reversexFactor = xMaxValue / 3f;
+        reverseyFactor = yMaxValue / 3f;
+
+        drawGraph = GraphObject.GetComponent<Sim_DrawGraph>();
+        ballTransform = ballObject.GetComponent<Transform>();
+        ballRigid = ballObject.GetComponent<Rigidbody>();
+        vibrationTimer = Time.fixedTime;
+        rotateTimer = Time.fixedTime;
+        moveUp = false;
+        tForm = GetComponent<Transform>();
+        rBody = GetComponent<Rigidbody>();
+        uiHandler = UIObject.GetComponent<Sim_UIHandler>();
+        blHandler = BallLocationHandlerObject.GetComponent<Sim_BallLocationHandler>();
+        bhHandler = BallHeightObject.GetComponent<Sim_BallHeightHandler>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Time.fixedTime >= vibrationTimer)
+        {
+            // Switch direction
+            if (!moveUp)
+            {
+                moveUp = true;
+            }
+            else
+            {
+                moveUp = false;
+            }
+
+            // Do the vibration
+            if (moveUp)
+            {
+                rBody.AddForce(new Vector3(0, 5f), ForceMode.VelocityChange);
+            }
+            else
+            {
+                rBody.AddForce(rBody.velocity * -2, ForceMode.VelocityChange);
+            }
+
+            vibrationTimer = Time.fixedTime + 0.05f;
+        }
+        if (Time.fixedTime >= rotateTimer)
+        {
+
+            // Balance the ball by velocity
+
+            if (ballRigid.velocity.x > 0)
+            {
+                transform.Rotate(0, 0, rotateVelocityValue);
+            }
+            if (ballRigid.velocity.x < 0)
+            {
+                transform.Rotate(0, 0, -rotateVelocityValue);
+            }
+
+            if (ballRigid.velocity.z > 0)
+            {
+                transform.Rotate(-rotateVelocityValue, 0, 0);
+            }
+
+            if (ballRigid.velocity.z < 0)
+            {
+                transform.Rotate(rotateVelocityValue, 0, 0);
+            }
+
+            // Balance the ball by position
+
+            if (ballTransform.position.x > transform.position.x)
+            {
+                transform.Rotate(0, 0, rotatePositionValue);
+            }
+
+            if (ballTransform.position.x < transform.position.x)
+            {
+                transform.Rotate(0, 0, -rotatePositionValue);
+            }
+
+            if (ballTransform.position.z > transform.position.z)
+            {
+                transform.Rotate(-rotatePositionValue, 0, 0);
+            }
+
+            if (ballTransform.position.z < transform.position.z)
+            {
+                transform.Rotate(rotatePositionValue, 0, 0);
+            }
+
+            rotateTimer = Time.fixedTime + 0.02f;
+        }
+    }
+    void Update()
+    {
+        // X -> X, Y -> Distance, Z -> Y
+
+        // X value
+        currentXConverted = originXValue + ballTransform.localPosition.x * reversexFactor;
+        // Distance value
+        currentYConverted = (ballTransform.localPosition.y - transform.localPosition.y) * reversedistanceFactor;
+        //Y value
+        currentZConverted = originYValue + ballTransform.localPosition.z * reverseyFactor;
+
+        drawGraph.UpdateGraphs(currentXConverted, currentZConverted, currentYConverted);
+        uiHandler.DisplayXYDistanceOriginXOriginY(currentXConverted, currentZConverted, currentYConverted, originXValue, originYValue);
+        blHandler.UpdateBallLocation(currentXConverted, currentZConverted);
+        bhHandler.UpdateBallHandler(currentYConverted);
+    }
+}
---
+++ Assets/Scripts/Simulation Mode/Sim_UIHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sim_UIHandler : MonoBehaviour
+{
+
+    [SerializeField] private GameObject ballObject;
+    [SerializeField] private GameObject plexiObject;
+    [SerializeField] private GameObject xObject;
+    [SerializeField] private GameObject yObject;
+    [SerializeField] private GameObject distanceObject;
+    [SerializeField] private GameObject originXObject;
+    [SerializeField] private GameObject originYObject;
+
+    private Transform ballTransform;
+    private Transform plexiTransform;
+    private Text positionXText;
+    private Text positionYText;
+    private Text DistanceText;
+
+    private Text originXText;
+    private Text originYText;
+
+    private float currentX;
+    private float currentY;
+    private float currentDistance;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballTransform = ballObject.GetComponent<Transform>();
+        plexiTransform = plexiObject.GetComponent<Transform>();
+        positionXText = xObject.GetComponent<Text>();
+        positionYText = yObject.GetComponent<Text>();
+        originXText = originXObject.GetComponent<Text>();
+        originYText = originYObject.GetComponent<Text>();
+        DistanceText = distanceObject.GetComponent<Text>();
+    }
+    public void DisplayXYDistanceOriginXOriginY(float X, float Y, float Distance, float originX, float originY)
+    {
+        positionXText.text = "Position X: " + X;
+        positionYText.text = "Position Y: " + Y;
+        DistanceText.text = "Distance: " + Distance;
+        originXText.text = "Origin X: " + originX;
+        originYText.text = "Origin Y: " + originY;
+    }
+}
